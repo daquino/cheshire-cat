5e02b7ce3089ce3e99501622f966d4720f9e0633
Daniel Aquino <daniel.j.aquino@gmail.com>
2016-06-01 23:55:43 -0500

    Add fade-out animation.

--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -3,11 +3,23 @@
   (:require [clojure.browser.repl :as repl]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
-            [enfocus.core :as ef]))
+            [enfocus.core :as ef]
+            [enfocus.events :as ev]
+            [enfocus.effects :as ee]))
+
+(defn say-goodbye []
+  (ef/at "#cat-name" (ee/fade-out 500))
+  (ef/at "#button1" (ee/fade-out 500))
+  (ef/at "#status" (ee/fade-out 5000)))
 
 (defn ^:export init []
   (go
     (let [response (<! (http/get "/cheshire-cat"))
           body   (-> (:body response) (js/JSON.parse) (js->clj))]
-         (ef/at "#cat-name" (ef/content (get body "name")))
-         (ef/at "#status" (ef/content (get body "status"))))))
+         (ef/at "#cat-name" (ef/content (get body "name"))
+                "#status" (ef/do->
+                            (ef/content (get body "status"))
+                            (ef/set-style :font-size "500%")))
+         (ef/at "#button1" (ev/listen
+                             :click
+                             #(say-goodbye))))))
